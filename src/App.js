28b81aeff82a5ef4d3f3a0002b2db335802280cc
dido <didoitk@abv.bg>
2023-01-31 14:21:45 +0200

    ToDo

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header title="Groceries List" />
+      <Header title="To Do List" />
       <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
